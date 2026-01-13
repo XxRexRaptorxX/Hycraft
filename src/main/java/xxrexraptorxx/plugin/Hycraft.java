a3d129dc8fc1e7a723ea3181320a17888aa46bfd
XxRexRaptorxX <Raptor-Simon@web.de>
2026-01-13 19:38:43 +0100

    chore: project setup improved

--- src/main/java/xxrexraptorxx/plugin/Hycraft.java
+++ src/main/java/xxrexraptorxx/plugin/Hycraft.java
@@ -1,6 +1,8 @@
-package org.example.plugin;
+package xxrexraptorxx.plugin;
 
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.command.system.CommandManager;
+import com.hypixel.hytale.server.core.console.ConsoleSender;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
@@ -10,11 +12,11 @@ import javax.annotation.Nonnull;
  * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
  * event listeners.
  */
-public class ExamplePlugin extends JavaPlugin {
+public class Hycraft extends JavaPlugin {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+    public Hycraft(@Nonnull JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
     }
@@ -22,6 +24,5 @@ public class ExamplePlugin extends JavaPlugin {
     @Override
     protected void setup() {
         LOGGER.atInfo().log("Setting up plugin " + this.getName());
-        this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
     }
 }
